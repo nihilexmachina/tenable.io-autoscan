c1f2bb0a8e14cc58fb56049d05813d995271439f
mixedup4x4 <mixedup4x4@users.noreply.github.com>
2023-02-09 17:56:42 -0500

    update tenableio_autoscan.py

--- tenableio_autoscan.py
+++ tenableio_autoscan.py
@@ -49,65 +49,42 @@ def add_agent():
     try:
         for agent in tio.agents.list(('groups', 'neq', '%s' % target_group)):
             full_list.append(agent)
-
-        # Search for Agents that DO NOT have the dict key "last_scanned" (a.k.a. never got scanned) and dumps their IDs to a new list
         for i in full_list:
             if key not in tio.agents.details(i['id']):
                 list_never_scanned.append(i)
-            else:
-                pass
-
-        # Checks if any agent needs to be added to the unscanned agent group based on the previous step. If not, execution stops
         agent_count = len(list_never_scanned)
         if agent_count == 0:
-            print("No Agent update required! Exiting...")
-            exit()
-        else:
-            pass
-
+            print("No agents to add. Exiting...")
+            return
         print("The following IDs never got scanned:")
         for i in list_never_scanned:
             print("Agent ID:", i['id'], "| Agent Name:", i['name'])
-
-        # Adds every non-scanned agent to an agent group that gets scanned every day
         for x in list_never_scanned:
             print("Adding Agent", x['id'], "(",
                   x['name'], ")", "to group", target_group)
             tio.agent_groups.add_agent(target_group, x['id'])
-
     except:
-        sys.exit("An error has ocurred attempting to add new Agents. Exiting...")
+        sys.exit("An error has occurred attempting to add new Agents. Exiting...")
 
 def delete_agent():
-    # If an agent belongs to group "Unscanned" and key last_scanned exists, remove agent from group
     print("Searching for Agents that got scanned...")
     try:
         for agent in tio.agents.list(('groups', 'eq', '%s' % target_group)):
             if key in tio.agents.details(agent['id']):
                 list_scanned.append(agent)
-            else:
-                pass
-
-        # Checks if any agent needs to be removed from the unscanned agent group based on the previous step. If not, execution stops
         agent_count = len(list_scanned)
         if agent_count == 0:
-            print("No Agent update required! Exiting...")
-            exit()
-        else:
-            pass
-
+            print("No agents to delete. Exiting...")
+            return
         print("The following IDs got scanned already:")
         for i in list_scanned:
             print("Agent ID:", i['id'], "| Agent Name:", i['name'])
-
-        # Deletes every scanned agent belonging to agent group Unscanned
         for i in list_scanned:
             print("Deleting Agent",
                   i['id'], "(", i['name'], ")", "from group", target_group)
             tio.agent_groups.delete_agent(target_group, i['id'])
-
     except:
-        sys.exit("An error has ocurred attempting to delete Agents. Exiting...")
+        sys.exit("An error has occurred attempting to delete Agents. Exiting...")
 
 def setup_logging(log_level, mask=False): #change to false to not mask the log entries or true to mask
     if mask:
@@ -132,7 +109,7 @@ def main():
     group.add_argument('--delete', action='store_const',
                        help='Deletes Nessus Agents from group if already scanned', const=delete_agent)
     parser.add_argument('--log', choices=['debug', 'info', 'warning', 'error', 'critical'],
-                        default='warning', help='Log level')
+                        default='info', help='Log level')
 
     args = parser.parse_args()
     log_level = getattr(logging, args.log.upper(), None)
